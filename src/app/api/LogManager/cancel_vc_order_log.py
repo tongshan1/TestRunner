0e4346fd079f7bd34365a4aab3860eda5be7cd18
耿返 <gengfan@tuhu.cn>
2018-05-15 21:49:09 +0800

    feat(CancelOrder): detail

--- src/app/api/LogManager/cancel_vc_order_log.py
+++ src/app/api/LogManager/cancel_vc_order_log.py
@@ -4,6 +4,9 @@ from flask import render_template, request
 from module.cancel_vc_order_log import CancelVcOrderLog
 from app.api import api
 from app.handler import register
+import pymssql
+import setting
+import json
 
 
 @register(api, "/cancel_vc_order_logs.html", methods=["GET"])
@@ -17,4 +20,16 @@ def cancel_vc_order_log_list():
 @register(api, "/cancel_vc_order_logs/<id>", methods=["GET"])
 def cancel_vc_order_log_list_detail(id):
     log = CancelVcOrderLog.query.get(id)
-    return render_template("cancel_vc_order_logs/detail.html", log=log)
+
+    conn = pymssql.connect(host=setting.CAR_PRODUCT_UAT_SERVER,
+                           port=int(setting.CAR_PRODUCT_UAT_PORT),
+                           user=setting.CAR_ORDER_UID,
+                           charset="utf8",
+                           password=setting.CAR_ORDER_PWD)
+
+    cursor = conn.cursor(as_dict=True)
+    cursor.execute("select OrderId,requestcontent,responsecontent,apirequestcontent,apiresponsecontent,errmsg from LOG_ConnectLog where OrderId={order_id} and operatetype=1".format(order_id=log.order_id))
+    detail = cursor.fetchone()
+    return render_template("cancel_vc_order_logs/detail.html", log=log, detail=json.dumps(detail))
+
+
