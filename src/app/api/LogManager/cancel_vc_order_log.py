7893de39e1885395e25191de1668b5eca92d1a57
耿返 <gengfan@tuhu.cn>
2018-05-15 16:30:10 +0800

    feat(pagination):  分页

--- src/app/api/LogManager/cancel_vc_order_log.py
+++ src/app/api/LogManager/cancel_vc_order_log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 from module.cancel_vc_order_log import CancelVcOrderLog
 from app.api import api
 from app.handler import register
@@ -8,8 +8,10 @@ from app.handler import register
 
 @register(api, "/cancel_vc_order_logs.html", methods=["GET"])
 def cancel_vc_order_log_list():
-    logs = CancelVcOrderLog.query.all()
-    return render_template("cancel_vc_order_logs/index.html", logs=logs)
+    page = request.args.get('page', 1, type=int)
+    pagination = CancelVcOrderLog.query.paginate(page, error_out=False)
+    logs = pagination.items
+    return render_template("cancel_vc_order_logs/index.html", pagination=pagination, logs=logs)
 
 
 @register(api, "/cancel_vc_order_logs/<id>", methods=["GET"])
