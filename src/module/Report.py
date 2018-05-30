e630ae49b33c25a5101aa582f386012dc7a8a25b
tongshan1 <1904736199@qq.com>
2018-05-30 18:23:28 +0800

    add 404 500

--- src/module/Report.py
+++ src/module/Report.py
@@ -77,6 +77,11 @@ class Report(db.Model):
     def testgroup(self, testgroup):
         self.testgroup_id = testgroup.id
 
+    @property
+    def during_time(self):
+        time = self.datachange_lasttime - self.datachange_createtime
+        return time
+
     @classmethod
     def all(cls):
         return cls.query.all()
